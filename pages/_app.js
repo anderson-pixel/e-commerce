d5c876f88d4e13c98a495d314123d76cd827fba6
anderson <anderson.ap133@gmail.com>
2022-06-21 22:44:59 -0300

    Updated Product Description Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Layout } from '../components';
+import { Toaster } from 'react-hot-toast';
 
+import { Layout } from '../components';
 import '../styles/globals.scss';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
+    </StateContext>
   )
 }
 
-export default MyApp;
+export default MyApp
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,16 +3,24 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  }
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
-        <div className="image-container">
+          <div className="image-container">
             <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
           <div className="small-images-container">
@@ -25,8 +33,9 @@ const ProductDetails = ({ product, products }) => {
               />
             ))}
           </div>
-          </div>
-          <div className="product-detail-desc">
+        </div>
+
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -46,14 +55,14 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick=''><AiOutlineMinus /></span>
-              <span className="num" onClick=''>0</span>
-              <span className="plus" onClick=''><AiOutlinePlus /></span>
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
             <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-            <button type="button" className="buy-now" onClick=''>Buy Now</button>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
       </div>
@@ -71,7 +80,7 @@ const ProductDetails = ({ product, products }) => {
     </div>
   )
 }
-        
+
 export const getStaticPaths = async () => {
   const query = `*[_type == "product"] {
     slug {
