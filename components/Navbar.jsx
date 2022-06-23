0181dc56f6869bfc4a900d2f5b149fb5a02cd349
anderson <anderson.ap133@gmail.com>
2022-06-22 23:08:59 -0300

    Updated Products List

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineShopping} from 'react-icons/ai'
 
 import { Cart } from './';
 import { useStateContext} from '../context/StateContext';
@@ -15,10 +15,10 @@ const Navbar = () => {
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-        <AiOutlineShopping />
+        <AiOutlineShopping/>
         <span className="cart-item-qty">{totalQuantities}</span>
       </button>
-
+      
       {showCart && <Cart />}
     </div>
   )
