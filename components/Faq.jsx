2b159d4bbd21dbb3b7bd83dc104bd1f1b5877d59
anderson <anderson.ap133@gmail.com>
2022-06-22 23:47:04 -0300

    Created Faq Component

---
+++ components/Faq.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+function Faq() {
+  return (
+    <div className='faq'>
+    <h1 classname="heading"> questions & <span>answers</span> </h1>
+    <div classname="accordion-container">
+    <div classname="accordion">
+        <div classname="accordion-heading">
+            <h3>lorem</h3>
+            <i classname="fas fa-angle-down"></i>
+        </div>
+        <p classname="accordioin-content">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, laboriosam non eligendi reiciendis quis laborum exercitationem voluptatibus autem harum nihil nisi sed mollitia, quam blanditiis architecto cumque? Sit, voluptate maiores.
+        </p>
+    </div>
+
+    <div classname="accordion">
+        <div classname="accordion-heading">
+            <h3>lorem</h3>
+            <i classname="fas fa-angle-down"></i>
+        </div>
+        <p classname="accordioin-content">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, laboriosam non eligendi reiciendis quis laborum exercitationem voluptatibus autem harum nihil nisi sed mollitia, quam blanditiis architecto cumque? Sit, voluptate maiores.
+        </p>
+    </div>
+
+    <div classname="accordion">
+        <div classname="accordion-heading">
+            <h3>lorem</h3>
+            <i classname="fas fa-angle-down"></i>
+        </div>
+        <p classname="accordioin-content">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, laboriosam non eligendi reiciendis quis laborum exercitationem voluptatibus autem harum nihil nisi sed mollitia, quam blanditiis architecto cumque? Sit, voluptate maiores.
+        </p>
+    </div>
+
+    <div classname="accordion">
+        <div classname="accordion-heading">
+            <h3>lorem</h3>
+            <i classname="fas fa-angle-down"></i>
+        </div>
+        <p classname="accordioin-content">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, laboriosam non eligendi reiciendis quis laborum exercitationem voluptatibus autem harum nihil nisi sed mollitia, quam blanditiis architecto cumque? Sit, voluptate maiores.
+        </p>
+    </div>
+
+    <div classname="accordion">
+        <div classname="accordion-heading">
+            <h3>lorem</h3>
+            <i classname="fas fa-angle-down"></i>
+        </div>
+        <p classname="accordioin-content">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, laboriosam non eligendi reiciendis quis laborum exercitationem voluptatibus autem harum nihil nisi sed mollitia, quam blanditiis architecto cumque? Sit, voluptate maiores.
+        </p>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Faq;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { client } from '../lib/client';
-import { Product, FooterBanner, HeroBanner } from '../components';
+import { Product, FooterBanner, HeroBanner, Faq } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
@@ -16,6 +16,7 @@ const Home = ({ products, bannerData }) => (
     </div>
 
     <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    <Faq/>
   </div>
 );
 
