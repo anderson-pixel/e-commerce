37a9d35a05dca04c5ced93b6861935068d00c38c
anderson <anderson.ap133@gmail.com>
2022-06-25 23:47:06 -0300

    Finished SideBar Component

---
+++ components/Banner.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+        <section class="banner">
+
+<div class="box-container">
+
+    <a href="#" class="box">
+        <img src="banner-1.jpg" alt=""/>
+        <div class="content">
+            <span>special offer</span>
+            <h3>upto 50% off</h3>
+        </div>
+    </a>
+
+    <a href="#" class="box">
+        <img src="banner-2.jpg" alt=""/>
+        <div class="content">
+            <span>special offer</span>
+            <h3>upto 50% off</h3>
+        </div>
+    </a>
+
+    <a href="#" class="box">
+        <img src="banner-3.jpg" alt=""/>
+        <div class="content">
+            <span>special offer</span>
+            <h3>upto 50% off</h3>
+        </div>
+    </a>
+    
+</div>
+
+</section>
+    </div>
+  )
+}
+
+export default Banner
