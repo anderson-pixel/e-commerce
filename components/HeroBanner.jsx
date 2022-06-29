e3cc5494dff9bf001b624e466122f5242794e21a
anderson <anderson.ap133@gmail.com>
2022-06-28 23:51:36 -0300

    Removed Swiper Flickering Bug

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Link from 'next/link';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation,EffectFade, Pagination, Scrollbar, A11y } from 'swiper';
 import Script from 'next/script';
 import styles from '../styles/Home.module.scss';
 
@@ -13,45 +13,48 @@ import styles from '../styles/Home.module.scss';
 import { urlFor } from '../lib/client';
 
 const HeroBanner = ({ heroBanner }) => {
+
+  const swiperNavPrevRef = useRef(null)
+  const swiperNavNextRef = useRef(null)
+
   return (
     <>
     <section className="home">
     <div className={styles.container}>
     <Swiper
-      spaceBetween={20}
+      modules={[Navigation, EffectFade]}
+
+      navigation={{
+        prevEl: swiperNavPrevRef.current,
+        nextEl: swiperNavNextRef.current,
+      }}
+      effect={'fade'}
+      speed={800}
       slidesPerView={1}
-      navigation
-      loop={true}
-      pagination={{ clickable: true}}
-      scrollbar={{ draggable: true}}
-      onSwiper={swiper => console.log('swiper')}
+      loop
       className={styles.myswiper}
+
+      onInit={(swiper) => {
+        swiper.params.navigation.prevEl = swiperNavPrevRef.current;
+        swiper.params.navigation.nextEl = swiperNavNextRef.current;
+        swiper.navigation.init();
+        swiper.navigation.update();
+      }}
     >
-    <div className="swipe-container">
-    <SwiperSlide className={styles.swiperslide}>
-        <div style={{display: 'flex',
-      alignItems: 'center',
-      flexWrap: 'wrap',
-      gap:'2rem',
-      minHeight: '40rem',
-      padding:'2rem 0'}}className="swiper-slide slide">
-
-            <div style={{flex: '1 1 42rem',textAlign: 'center'}} className="image">
-                <img style={{height: '30rem'}} src="home-img-1.jpg" alt=""/>
-            </div>
-            <div className="content" style={{flex:'1 1 42rem'}}>
-                <span style={{fontSize: '2rem',
-          color:'blue'}}>upto 50% off</span>
-                <h3 style={{paddingTop: '.5rem',
-          color: 'black',
-          fontSize: '3rem'}}>headphones</h3>
-                <a href="#" className="btn">shop now</a>
-            </div>
-        </div>
-
-        </SwiperSlide>
-      </div>
-    </Swiper>
+      <SwiperSlide className={styles.swiperslide}>
+        <img src='home-img-1.jpg' alt='' />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperslide}>
+        <img src='home-img-2.jpg' alt='' />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperslide}>
+        <img src='home-img-3.jpg' alt='' />
+      </SwiperSlide>
+      <div className={styles.swiperNavPrev} ref=
+      {swiperNavPrevRef}></div>
+      <div className={styles.swiperNavNext} ref=
+      {swiperNavNextRef}></div>
+   </Swiper>
     </div>
     </section>
     </>
